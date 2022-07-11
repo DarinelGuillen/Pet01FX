116b08c1186d6a78787d8b1963d345db255c5340
Darinel <darinel.escoba@outlook.com>
2022-07-11 00:31:53 -0500

    Push DArinel modifique/agregue x o y cosas

---
+++ src/main/java/com/petmaps/aplication/controllers/FirstController.java
@@ -0,0 +1,70 @@
+package com.petmaps.aplication.controllers;
+
+import com.petmaps.aplication.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class FirstController {
+
+    @FXML
+    private ImageView M;
+
+    @FXML
+    private Button btnInicio;
+
+    @FXML
+    private Button btnMaps;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Button btnServicios;
+
+    @FXML
+    private Button btnSobreNosotros;
+
+    @FXML
+    private Button btnUneteAs;
+
+    @FXML
+    private Button btnggggg;
+
+    @FXML
+    void btnInicioOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnMapsOnMouseClicked(MouseEvent event) {
+        HelloApplication.setFXML("Servicios-view", "maps");
+
+    }
+
+    @FXML
+    void btnNosotrosOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnServiciosOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnUneteAsOnMouseClicked(MouseEvent event) {
+        HelloApplication.setFXML("user-view", "Change user - controll");
+
+    }
+
+    @FXML
+    void btngggggOnMauseClicked(MouseEvent event) {
+
+    }
+
+}
+
+
+// HelloApplication.setFXML("user-view", "Change user - controll");
---
+++ src/main/java/com/petmaps/aplication/controllers/ServisiosController.java
@@ -0,0 +1,19 @@
+package com.petmaps.aplication.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ServisiosController {
+
+    @FXML
+    private Button btnHola;
+
+    @FXML
+    void btnHolaOnMouseClicked(MouseEvent event) {
+
+
+    }
+
+}
+
