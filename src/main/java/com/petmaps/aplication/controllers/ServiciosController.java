5c3dfe548bb39a6893df06a2b120fd2faa2a7582
Darinel <darinel.escoba@outlook.com>
2022-07-18 12:22:21 -0500

    Modifico Chandoqui

--- src/main/java/com/petmaps/aplication/controllers/ServiciosController.java
+++ src/main/java/com/petmaps/aplication/controllers/ServiciosController.java
@@ -1,10 +1,14 @@
 package com.petmaps.aplication.controllers;
 
+import com.petmaps.aplication.HelloApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 public class ServiciosController {
+
+    @FXML
+    private Button btnInicio;
 
     @FXML
     private Button btnRefugio;
@@ -15,6 +19,11 @@ public class ServiciosController {
     @FXML
     private Button btnVeterinaria;
 
+    @FXML
+    void btnInicioOnClickedMouse(MouseEvent event) {
+        HelloApplication.setFXML("First-view", "Regreso de ServiSios ");
+
+    }
     @FXML
     void bntVeterinarioOnMOuseClicked(MouseEvent event) {
 
